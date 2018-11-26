f83f1b5ce5dac7d1b440a02356fe9708967eb0ed
Muna <mmhussein125@gmail.com>
2018-11-26 17:36:30 +0000

    adds post req from contact form

--- app/javascript/components/Contact.js
+++ app/javascript/components/Contact.js
@@ -1,18 +1,25 @@
 import React from 'react'
 
 class Contact extends React.Component {
+  handleSubmit(e) {
+    let message = e.target.elements[0].value
+    axios.post('/contact', `message=${message}`)
+    .then( (data)=> {
+      debugger
+    })
+  }
   render(){
     return(
       <div className="container" style={{padding: '40px 0'}}>
-      <div className="row">
-      <div className="col-md-8 offset-md-2">
-      <h1>Contact Us.</h1>
-      <form>
-      <input type="text" name="message" class="form-control" placeholder="Send Us a Message!"/>
-      <button class="btn btn-primary" type="submit">Contact Us</button>
-      </form>
-      </div>
-      </div>
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <h1>Contact Us.</h1>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <input type="text" name="message" className="form-control" placeholder="Send Us a Message!"/>
+              <button className="btn btn-primary" type="submit">Contact Us</button>
+            </form>
+          </div>
+        </div>
       </div>
 
     )
