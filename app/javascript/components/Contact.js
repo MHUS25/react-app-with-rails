2204b79d0b3b445b6708e6e14a9beeb31cf83073
Muna <mmhussein125@gmail.com>
2018-11-26 16:30:35 +0000

    adds bootstrap and starts on contact functionality

--- app/javascript/components/Contact.js
+++ app/javascript/components/Contact.js
@@ -3,7 +3,18 @@ import React from 'react'
 class Contact extends React.Component {
   render(){
     return(
-      <div>This is the Contact page.</div>
+      <div className="container" style={{padding: '40px 0'}}>
+      <div className="row">
+      <div className="col-md-8 offset-md-2">
+      <h1>Contact Us.</h1>
+      <form>
+      <input type="text" name="message" class="form-control" placeholder="Send Us a Message!"/>
+      <button class="btn btn-primary" type="submit">Contact Us</button>
+      </form>
+      </div>
+      </div>
+      </div>
+
     )
   }
 }
